63e3d9887ef3f2fc6bbacef2b66038cf7f1eae61
GereDaniel <geredani9b@gmail.com>
2024-10-18 09:32:23 +0200

    reszponzivitas 1

--- src/pages/Admin/admin.js
+++ src/pages/Admin/admin.js
@@ -1,3 +1,10 @@
   function openSidebar() {
     document.querySelector(".sidebar").classList.toggle("hidden");
   }
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("closenav").style.width = "0";
+  }
