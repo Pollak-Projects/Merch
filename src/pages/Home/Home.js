dc69ded14752fa673feb2544a057e493b9542f1b
FaboRobi <fabodudasr@gmail.com>
2024-12-06 13:04:43 +0100

    JS backend & Some CSS Fixing
    I did everything for the Js Handeling and Prepaited the Data to sned to the "Kosár"
    Fixed the + and - to be center And moved the Colum-3 down

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,3 @@
-/* Start Column-1 */
-
 /* Slideshow controll */ 
 
 let slideIndex = 1;
@@ -32,4 +30,86 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-/* End Column-1 */
+/* Polo Data Handeling */
+
+//These will go to Kosár
+let poloAmount = 1;
+let poloSize = 0;
+let poloColor = 0;
+
+//Amount Of Polo Controller
+
+let PoloPrice = 3000;
+
+function AmountOfPoloController(a) {
+
+  let dispalyAmount = document.getElementById("AmountOfPolo");
+  let displayPrice = document.getElementById("PoloPrice");
+
+  if(a == 1 && poloAmount < 10){
+    poloAmount++;
+    PoloPrice += 3000;
+  }
+
+  if(a == -1 && poloAmount > 1){
+    poloAmount--;
+    PoloPrice -= 3000;
+  }
+
+  dispalyAmount.innerHTML = poloAmount;
+  displayPrice.innerHTML= PoloPrice + " Ft";
+}
+
+// Polo Size Changling
+
+function SizeChagning(s){
+  if(s == 1){
+    poloSize = "S";
+  }
+  else if(s == 2){
+    poloSize = "M";
+  }
+  else if(s == 3){
+    poloSize = "L";
+  }
+  else if(s == 4){
+    poloSize = "XL";
+  }
+  else if(s == 5){
+    poloSize = "XXL";
+  }
+}
+
+// Polo Color Changing
+
+function ColorChanging(c){
+  if(c == 1){
+    poloColor = "Yellow";
+  }
+  else if(c == 2){
+    poloColor = "Red";
+  }
+  else if(c == 3){
+    poloColor = "Blue";
+  }
+  else if(c == 4){
+    poloColor = "Black";
+  }
+}
+
+// These will Check for empty Data and send everything to "Kosár"
+
+function SendDataToKosar(){
+  if(poloColor == 0){
+    alert("Válasz még egy Színt is")
+  }
+  else if(poloSize == 0){
+    alert("Válasz még egy Méretet is")
+  }
+  else{
+    console.log(poloAmount);
+    console.log(poloSize);
+    console.log(poloColor);
+  }
+  
+}
